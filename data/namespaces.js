a00e1d40b0d1ecc2bee4bf30c2c2dc5582f21637
Jayprakash Das <jay.rgu.123@gmail.com>
2019-08-08 17:58:09 +0530

    fc2

--- data/namespaces.js
+++ data/namespaces.js
@@ -7,8 +7,9 @@ let namespaces = [];
 let wikiNs = new Namespace(0,'Wiki','https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png','/wiki');
 let mozNs = new Namespace(1,'Mozilla','https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png','/mozilla');
 let linuxNs = new Namespace(2,'Linux','https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png','/linux');
+let microsoft = new Namespace(3,'Microsoft',"http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Microsoft-Logo-PNG-Transparent-1.png",'/microsoft');
 
-namespaces.push(wikiNs,mozNs,linuxNs);
+namespaces.push(wikiNs,mozNs,linuxNs,microsoft);
 
 // Make the main room and add it to rooms. it will ALWAYS be 0
 wikiNs.addRoom(new Room(0,'New Articles','Wiki'));
@@ -25,4 +26,8 @@ linuxNs.addRoom(new Room(1,'Red Hat','Linux'));
 linuxNs.addRoom(new Room(2,'MacOs','Linux'));
 linuxNs.addRoom(new Room(3,'Kernal Development','Linux'));
 
+microsoft.addRoom(new Room(0,'Product','Microsoft'));
+microsoft.addRoom(new Room(1,'Office Suite','Microsoft'));
+microsoft.addRoom(new Room(2,'Windows 10 pro','Microsoft'));
+microsoft.addRoom(new Room(3,'Benz Suite','Microsoft'));
 module.exports = namespaces;
